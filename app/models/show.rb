b1e592d54d7dcefa5978e505c93e69d2ed7ccd46
Derek Araujo <derekaraujo19@gmail.com>
2022-07-06 15:24:00 +0200

    commit tree 731709d9cc83141ca937903ad629040a1a90e215

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    actor_list = self.characters.map do |charac|
+      charac.actor.full_name
+    end
+    actor_list
+  end
+
 end
+
+
+
+
+#actors_list: Define a method in the Show class called #actors_list that returns an Array of the full names of each actor associated with a show. Remember, a show should have many actors through characters. Hint: it may help to use another association macro to access the actors for the show.
